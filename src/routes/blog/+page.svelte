075d358beceea57958a2ff21b0bc3fab8e5ce0cf
Eugenio <esarrit@gmail.com>
2023-04-06 00:21:31 -0400

    remove top level #if from pages after regression

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -23,42 +23,40 @@
   <meta property="og:image" content={eugenio} />
 </svelte:head>
 
-{#if isPageLoaded}
-  {#key isPageLoaded}
-    <div class="relative flex w-full" in:fly={{ delay: 150, duration: 200, x: 0, y: 50, opacity: 0, easing: sineIn }}>
-      <div class="flex flex-col">
-        <h1 class="mx-auto mb-6 mt-2 text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-300 xs:mt-4 md:text-3xl">Posts</h1>
-        <span class="mb-6 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
-          Soon, this will be a place where I share my perspective on things I've learned. In the meantime, it's just another chance to show off my domesticated
-          fox.
-        </span>
-        <ImageWithLoadBlur
-          alt="Reina & me working hard."
-          smallImage={reinaMemeSmall}
-          originalImage={reinaMeme}
-          imageClass="h-auto w-[80vw] rounded-xl opacity-100 xs:w-[60vw] xl:w-[40vw]"
-        />
-        <!--
+{#key isPageLoaded}
+  <div class="relative flex w-full" in:fly={{ delay: 150, duration: 200, x: 0, y: 50, opacity: 0, easing: sineIn }}>
+    <div class="flex flex-col">
+      <h1 class="mx-auto mb-6 mt-2 text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-300 xs:mt-4 md:text-3xl">Posts</h1>
+      <span class="mb-6 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
+        Soon, this will be a place where I share my perspective on things I've learned. In the meantime, it's just another chance to show off my domesticated
+        fox.
+      </span>
+      <ImageWithLoadBlur
+        alt="Reina & me working hard."
+        smallImage={reinaMemeSmall}
+        originalImage={reinaMeme}
+        imageClass="h-auto w-[80vw] rounded-xl opacity-100 xs:w-[60vw] xl:w-[40vw]"
+      />
+      <!--
       <div class="flex w-full justify-center">
         <img alt="Reina & me." src={reinaMeme} decoding="async" class="h-auto w-[80vw] rounded-xl opacity-100 xs:w-[60vw] xl:w-[40vw]" />
       </div>
       -->
-        <span class="mx-auto !mt-3 mb-6 text-sm text-gray-500 dark:text-zinc-400">This is a real domesticated fox</span>
-        <span class="mb-1.5 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
-          Here are some topics I want to write about:
-          <ul>
-            <li>1. <span class="font-medium underline">My journey in tech</span>: a recap of my career and lessons learned.</li>
-            <li>
-              2. <span class="font-medium underline">Consuming vs. Creating</span>: spending less time online consuming and pushing myself to make things.
-            </li>
-            <li>
-              3. <span class="font-medium underline">Overthinking, Overplanning, & Imposter Syndrome</span>: how simple things can be hard.
-            </li>
-            <li>4. <span class="font-medium underline">Artificial Intelligence</span>: what I've learned so far from others and from experiments of my own.</li>
-            <li>5. <span class="font-medium underline">Current Tech Stack</span>: the technologies I chose to learn to build this site + other projects.</li>
-          </ul>
-        </span>
-      </div>
+      <span class="mx-auto !mt-3 mb-6 text-sm text-gray-500 dark:text-zinc-400">This is a real domesticated fox</span>
+      <span class="mb-1.5 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
+        Here are some topics I want to write about:
+        <ul>
+          <li>1. <span class="font-medium underline">My journey in tech</span>: a recap of my career and lessons learned.</li>
+          <li>
+            2. <span class="font-medium underline">Consuming vs. Creating</span>: spending less time online consuming and pushing myself to make things.
+          </li>
+          <li>
+            3. <span class="font-medium underline">Overthinking, Overplanning, & Imposter Syndrome</span>: how simple things can be hard.
+          </li>
+          <li>4. <span class="font-medium underline">Artificial Intelligence</span>: what I've learned so far from others and from experiments of my own.</li>
+          <li>5. <span class="font-medium underline">Current Tech Stack</span>: the technologies I chose to learn to build this site + other projects.</li>
+        </ul>
+      </span>
     </div>
-  {/key}
-{/if}
+  </div>
+{/key}
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -23,51 +23,49 @@
   <meta property="og:image" content={eugenio} />
 </svelte:head>
 
-{#if isPageLoaded}
-  {#key isPageLoaded}
-    <div class="relative flex w-full" in:fly={{ delay: 150, duration: 200, x: 0, y: 50, opacity: 0, easing: sineIn }}>
-      <div class="flex flex-col">
-        <h1 class="mx-auto mb-6 mt-2 text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-300 xs:mt-4 md:text-3xl">Projects</h1>
-        <span class="mb-6 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300"
-          >As I build and ship projects, I will link them in this page. In the meantime, here's a live look at how I'm spending my nights and weekends.
-        </span>
-        <ImageWithLoadBlur
-          alt="Reina & me working hard."
-          smallImage={eugenioReinaSmall}
-          originalImage={eugenioReina}
-          imageClass="h-auto w-[80vw] rounded-xl opacity-100 xs:w-[60vw] xl:w-[40vw]"
-        />
-        <!--
+{#key isPageLoaded}
+  <div class="relative flex w-full" in:fly={{ delay: 150, duration: 200, x: 0, y: 50, opacity: 0, easing: sineIn }}>
+    <div class="flex flex-col">
+      <h1 class="mx-auto mb-6 mt-2 text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-300 xs:mt-4 md:text-3xl">Projects</h1>
+      <span class="mb-6 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300"
+        >As I build and ship projects, I will link them in this page. In the meantime, here's a live look at how I'm spending my nights and weekends.
+      </span>
+      <ImageWithLoadBlur
+        alt="Reina & me working hard."
+        smallImage={eugenioReinaSmall}
+        originalImage={eugenioReina}
+        imageClass="h-auto w-[80vw] rounded-xl opacity-100 xs:w-[60vw] xl:w-[40vw]"
+      />
+      <!--
       <div class="flex w-full justify-center blur-md">
         <img alt="Reina & me working hard." src={eugenioReina} decoding="async" class="h-auto w-[80vw] rounded-xl opacity-100 xs:w-[60vw] xl:w-[40vw]" />
       </div> -->
-        <span class="mx-auto !mt-3 mb-6 text-sm text-gray-500 dark:text-zinc-400">Working hard with my secret assistant</span>
-        <span class="mx-auto mb-6 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
-          As many others who follow the technology industry, I am amazed by the modern wave of Artificial Intelligence. Since October 2022, I've spent countless
-          hours on Twitter and YouTube learning about topics that spark my interests in the incredible world of AI.
-        </span>
-        <div class="mb-6 flex justify-around rounded bg-zinc-200/30 shadow-lg shadow-zinc-800/5 dark:bg-zinc-800/75">
-          <span class="p-2 text-left text-base font-medium tracking-tight text-zinc-500 dark:text-zinc-400 xs:text-base">
-            During the rest of 2023, I want to spend more of my time online <em>creating</em> instead of <em>consuming</em>.
-          </span>
-        </div>
-        <span class="mb-1.5 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
-          Here are some of the ideas I want to turn into reality this year:
-          <ul>
-            <li>1. <span class="font-medium underline">This site</span>: continue developing my personal site & begin writing online in a blog format.</li>
-            <li>2. <span class="font-medium underline">ChatCV</span>: chat with my resume & CV in a ChatGPT-type interface.</li>
-            <li>
-              3. <span class="font-medium underline">ProjectAssist</span>: create a GPT-4 based personal assistant that has up to date information about my
-              technology stack of choice (e.g. Svelte, SvelteKit, TailwindCSS, ,TypeScript, etc.) to help me build side projects faster.
-            </li>
-            <li>
-              4. <span class="font-medium underline">PuertoRico.ai</span>: allow people to explore the culture, history, and places of our incredible island
-              using text-to-image and large language models.
-            </li>
-            <li>5. <span class="font-medium underline">Teaching</span>: develop ways to help others learn.</li>
-          </ul>
+      <span class="mx-auto !mt-3 mb-6 text-sm text-gray-500 dark:text-zinc-400">Working hard with my secret assistant</span>
+      <span class="mx-auto mb-6 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
+        As many others who follow the technology industry, I am amazed by the modern wave of Artificial Intelligence. Since October 2022, I've spent countless
+        hours on Twitter and YouTube learning about topics that spark my interests in the incredible world of AI.
+      </span>
+      <div class="mb-6 flex justify-around rounded bg-zinc-200/30 shadow-lg shadow-zinc-800/5 dark:bg-zinc-800/75">
+        <span class="p-2 text-left text-base font-medium tracking-tight text-zinc-500 dark:text-zinc-400 xs:text-base">
+          During the rest of 2023, I want to spend more of my time online <em>creating</em> instead of <em>consuming</em>.
         </span>
       </div>
+      <span class="mb-1.5 text-left text-base font-medium tracking-tight text-zinc-600 dark:text-zinc-300">
+        Here are some of the ideas I want to turn into reality this year:
+        <ul>
+          <li>1. <span class="font-medium underline">This site</span>: continue developing my personal site & begin writing online in a blog format.</li>
+          <li>2. <span class="font-medium underline">ChatCV</span>: chat with my resume & CV in a ChatGPT-type interface.</li>
+          <li>
+            3. <span class="font-medium underline">ProjectAssist</span>: create a GPT-4 based personal assistant that has up to date information about my
+            technology stack of choice (e.g. Svelte, SvelteKit, TailwindCSS, ,TypeScript, etc.) to help me build side projects faster.
+          </li>
+          <li>
+            4. <span class="font-medium underline">PuertoRico.ai</span>: allow people to explore the culture, history, and places of our incredible island using
+            text-to-image and large language models.
+          </li>
+          <li>5. <span class="font-medium underline">Teaching</span>: develop ways to help others learn.</li>
+        </ul>
+      </span>
     </div>
-  {/key}
-{/if}
+  </div>
+{/key}
