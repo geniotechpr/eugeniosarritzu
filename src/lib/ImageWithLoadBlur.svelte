92c6bcfb46382b09bc8e0d121fc4eb67d20015c0
Eugenio <esarrit@gmail.com>
2023-04-06 01:32:11 -0400

    small bug fix after for image blur component

--- src/lib/ImageWithLoadBlur.svelte
+++ src/lib/ImageWithLoadBlur.svelte
@@ -16,7 +16,7 @@
   let isLoadingBlurVisible = true;
 
   function makeLoadingBlurInvisible() {
-    isLoadingBlurVisible = true;
+    isLoadingBlurVisible = false;
   }
 
   // On Component Mount, set up animations and return cleanup function for unmount
